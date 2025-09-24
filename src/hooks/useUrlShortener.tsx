4dd7331464327f2d3477ff01d378cc6b9e567120
Travis Adams <brokenCD@live.com>
2025-09-24 22:20:40 +0700

    create hook to call url shortener

---
+++ src/hooks/useUrlShortener.tsx
@@ -0,0 +1,49 @@
+import { useRef, useState } from 'react';
+import type { ShortenedURL } from '../models/ShortenedURL';
+
+const useUrlShortener = () => {
+    const [loading, setLoading] = useState<boolean>(false);
+    const [data, setData] = useState<ShortenedURL | null>(null);
+    const [error, setError] = useState(null);
+    const baseURL = 'https://localhost:8080/';
+    const abortControllerRef = useRef<AbortController | null>(null);
+
+    const shorten = async (longURL: string) => {
+        if (!longURL.trim()) return;
+
+        if (abortControllerRef.current) {
+            abortControllerRef.current.abort();
+        }
+
+        const controller = new AbortController();
+        abortControllerRef.current = controller;
+
+        setLoading(true);
+        setError(null);
+
+        try {
+            const res = await fetch(
+                `${baseURL}shorten?long_url=${encodeURIComponent(longURL)}`,
+                { signal: controller.signal }
+            )
+
+            if (!res.ok) {
+                throw new Error(`Request failed with status: ${res.status}`)
+            }
+
+            const result: ShortenedURL = await res.json();
+            setData(result);
+        } catch(err: any) {
+            if (err.name !== 'AbortError') {
+                console.error(err);
+                setError(err);
+            }
+        } finally {
+            setLoading(false);
+        }
+    }
+
+    return { loading, data, error, shorten };
+}
+
+export default useUrlShortener;
---
+++ src/models/ShortenedURL.ts
@@ -0,0 +1,6 @@
+export interface ShortenedURL {
+    id: number;
+    short_code: string;
+    long_url: string;
+    created_at: Date;
+}
